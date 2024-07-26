ef3f54971a03932a4716217cadfa19cd8cf57f74
Timmy <timmy.the.nobody@gmail.com>
2024-07-26 11:41:45 +0200

    style (navbar): files organization

--- lua/tlib2/client/vgui_controls/navbar.lua
+++ lua/tlib2/client/vgui_controls/navbar.lua
@@ -1,81 +1,148 @@
-local PANEL = {}
-
 local iBarH = 2 -- The height of the bar at the top of the navbar
 
-function PANEL:Init()
-    self.buttons = {}
-    self.selected = 1
+----------------------------------------------------------------------
+-- Sheet Tab
+----------------------------------------------------------------------
 
-    self:SetTall((ScrH() * 0.03) + iBarH)
+local PANEL = {}
+
+function PANEL:Init()
+    self:SetCursor("hand")
+    self:SetFont("TLib2.6")
+    self:SetTextColor(TLib2.Colors.Base4)
+    self:SetTextInset(0, -iBarH)
+
+    self.lerp_select = 0
+end
+
+function PANEL:GetNavbar()
+    return self.navbar
+end
+
+function PANEL:SetNavbar(dNavbar)
+    self.navbar = dNavbar
+
+    -- self:SetTall(dNavbar.tabs_container:GetTall())
+end
+
+function PANEL:GetIndex()
+    return self.index or -1
+end
+
+function PANEL:SetIndex(iIndex)
+    self.index = iIndex
+end
+
+function PANEL:DoClick()
+    local dNavbar = self:GetNavbar()
+    if not dNavbar or not dNavbar:IsValid() then return end
+
+    dNavbar:SetSelected(self:GetIndex())
+end
+
+function PANEL:Paint(iW, iH)
+    local dNavbar = self:GetNavbar()
+    if not dNavbar or not dNavbar:IsValid() then return end
+
+    -- Draw the bar
+    surface.SetDrawColor(TLib2.Colors.Base2)
+    surface.DrawRect(0, (iH - iBarH), iW, iBarH)
+
+    if (dNavbar:GetSelected() == self:GetIndex()) then
+        self.lerp_select = Lerp(RealFrameTime() * 16, self.lerp_select, 1)
+
+        surface.SetDrawColor(TLib2.Colors.Accent)
+        surface.DrawRect((iW - (self.lerp_select * iW)) * 0.5, (iH - iBarH), self.lerp_select * iW, iBarH)
+
+            -- self:SetTextColor(TLib2.Colors.Base4)
+    else
+        if (self.lerp_select == 0) then return end
+
+        self.lerp_select = 0
+        -- self:SetTextColor(TLib2.Colors.Base3)
+    end
+end
+
+vgui.Register("TLib2:NavbarTab", PANEL, "DButton")
+
+----------------------------------------------------------------------
+-- Sheet
+----------------------------------------------------------------------
+
+local PANEL = {}
+
+function PANEL:Init()
+    self.tabs = {}
+
+    self.tabs_container = self:Add("DPanel")
+    self.tabs_container:Dock(TOP)
+    self.tabs_container:DockMargin(0, 0, 0, TLib2.Padding4)
+    self.tabs_container:SetTall((ScrH() * 0.03) + iBarH)
+    self.tabs_container.Paint = nil
 end
 
 function PANEL:GetSelected()
     return self.selected
 end
 
-function PANEL:SetSelected(iButton)
-    if not self.buttons[iButton] then return end
-    self.selected = iButton
-end
+function PANEL:SetSelected(iTab)
+    if not self.tabs[iTab] then return end
 
-function PANEL:AddButton(sLabel, fnOnClick)
-    local dNavBar = self
-    local iID = (#self.buttons + 1)
-
-    self.buttons[iID] = {
-        id = iID,
-        label = sLabel,
-        on_click = fnOnClick
-    }
-
-    local dButton = self:Add("DButton")
-    self.buttons[iID].vgui = dButton
-
-    dButton:Dock(LEFT)
-    dButton:SetTall(self.button_height)
-    dButton:SetText(sLabel or "")
-    dButton:SetCursor("hand")
-    dButton:SetFont("TLib2.6")
-    dButton:SetTextColor(TLib2.Colors.Base3)
-    dButton:SetTextInset(0, -iBarH)
-    dButton.lerp_select = 0
-
-    function dButton:Paint(iW, iH)
-        surface.SetDrawColor(TLib2.Colors.Base2)
-        surface.DrawRect(0, (iH - iBarH), iW, iBarH)
-
-        if (dNavBar:GetSelected() == iID) then
-            self.lerp_select = Lerp(RealFrameTime() * 16, self.lerp_select, 1)
-
-            surface.SetDrawColor(TLib2.Colors.Accent)
-            surface.DrawRect((iW - (self.lerp_select * iW)) * 0.5, (iH - iBarH), self.lerp_select * iW, iBarH)
-
-            self:SetTextColor(TLib2.Colors.Base4)
-        else
-            if (self.lerp_select == 0) then return end
-
-            self.lerp_select = 0
-            self:SetTextColor(TLib2.Colors.Base3)
+    local iOld = self:GetSelected()
+    if self.tabs[iOld] then
+        local dOldPanel, dOldButton = self.tabs[iOld].panel, self.tabs[iOld].button
+        if dOldPanel and dOldPanel:IsValid() then
+            dOldPanel:SetVisible(false)
+        end
+        if dOldButton and dOldButton:IsValid() then
+            dOldButton:SetTextColor(TLib2.Colors.Base3)
         end
     end
 
-    function dButton:DoClick()
-        if fnOnClick then
-            local bReturn = fnOnClick(self, dNavBar.buttons[iID])
-            if (bReturn == false) then return end
-        end
+    local dNewPanel, dNewButton = self.tabs[iTab].panel, self.tabs[iTab].button
+    if dNewPanel and dNewPanel:IsValid() then
+        dNewPanel:SetVisible(true)
+        dNewPanel:Dock(FILL)
+    end
+    if dNewButton and dNewButton:IsValid() then
+        dNewButton:SetTextColor(TLib2.Colors.Base4)
+    end
 
-        dNavBar:SetSelected(iID)
+    self.selected = iTab
+end
+
+function PANEL:AddTab(sLabel, dPanel, bSelected)
+    local iTab = (#self.tabs + 1)
+
+    dPanel:SetVisible(false)
+    dPanel:SetParent(self)
+
+    self.tabs[iTab] = {
+        id = iTab,
+        label = sLabel,
+        panel = dPanel,
+        button = self.tabs_container:Add("TLib2:NavbarTab")
+    }
+
+    local dButton = self.tabs[iTab].button
+    dButton:Dock(LEFT)
+    dButton:SetText(sLabel or "")
+    dButton:SetIndex(iTab)
+    dButton:SetNavbar(self)
+
+    if not self:GetSelected() or bSelected then
+        self:SetSelected(iTab)
     end
 end
 
 function PANEL:PerformLayout(iW, iH)
-    for i = 1, #self.buttons do
-        self.buttons[i].vgui:SetWide(iW / #self.buttons)
+    for i = 1, #self.tabs do
+        self.tabs[i].button:SetWide(iW / #self.tabs)
     end
 end
 
 function PANEL:Paint(iW, iH)
+    -- draw.RoundedBox(TLib2.BorderRadius, 0, 0, iW, iH, TLib2.Colors.Base1)
 end
 
 vgui.Register("TLib2:Navbar", PANEL, "DPanel")
