2e36fd7b1ef0b3905d4ad4726764e75d1bb61cff
Timmy <timmy.the.nobody@gmail.com>
2024-08-19 18:28:24 +0200

    feat/perf (tooltip): add `o:GetAnchor`, reduce calls

--- lua/tlib2/client/vgui_controls/tooltip.lua
+++ lua/tlib2/client/vgui_controls/tooltip.lua
@@ -1,6 +1,8 @@
 local PANEL = {}
 
 local draw = draw
+local surface = surface
+local DisableClipping = DisableClipping
 
 function PANEL:Init()
     self:SetDrawOnTop(true)
@@ -13,19 +15,25 @@ function PANEL:Init()
     self.padding = TLib2.Padding3
 end
 
-function PANEL:SetAnchor(dPanel)
-    self.anchor = dPanel
-    
-    local dTooltip = self
-    local fnOldOnRemove = dPanel.OnRemove
+function PANEL:GetAnchor()
+    return self.anchor
+end
 
-    function dPanel:OnRemove()
-        dTooltip:Remove()
+function PANEL:SetAnchor(dPanel)
+    self:Hide()
+    self.anchor = dPanel
+
+    local fnOldOnRemove = dPanel.OnRemove
+    dPanel.OnRemove = function(_)
+        self:Remove()
 
         if (type(fnOldOnRemove) == "function") then
-            fnOldOnRemove(self)
+            fnOldOnRemove(dPanel)
         end
     end
+
+    self:InvalidateLayout(true)
+    self:Show()
 end
 
 function PANEL:Paint(iW, iH)
@@ -44,18 +52,17 @@ function PANEL:PerformLayout(iW, iH)
     surface.SetFont(self:GetFont())
     local iTextW, iTextH = surface.GetTextSize(self:GetText())
 
-    if (iTextW > (ScrH() * 0.2)) then
-        self:SetWide(ScrH() * 0.2)
-    else
-        self:SetWide(iTextW)
-    end
+    local iMaxW = (ScrH() * 0.2)
+    local iNewW = (iTextW > iMaxW) and iMaxW or iTextW
+
+    self:SetWide(iNewW)
 
     if not self.anchor or not self.anchor:IsValid() then return end
 
     local iX, iY = self.anchor:LocalToScreen(0, 0)
     iX = iX + (self.anchor:GetWide() * 0.5)
 
-    self:SetPos(iX - (self:GetWide() * 0.5), iY - self:GetTall() - (TLib2.Padding4 * 2))
+    self:SetPos(iX - (iNewW * 0.5), iY - self:GetTall() - (TLib2.Padding4 * 2))
 end
 
 vgui.Register("TLib2:Tooltip", PANEL, "DLabel")
