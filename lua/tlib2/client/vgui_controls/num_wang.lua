d766371a06dc574f16917db2d6f470c8bd7617fe
Timmy <timmy.the.nobody@gmail.com>
2024-09-25 04:19:08 +0200

    perf (num wang): optimize `o:PerformLayout`

--- lua/tlib2/client/vgui_controls/num_wang.lua
+++ lua/tlib2/client/vgui_controls/num_wang.lua
@@ -6,18 +6,16 @@ function PANEL:Init()
     self:SetTextColor(TLib2.Colors.Base4)
     self:SetDrawLanguageID(false)
     self:SetPaintBackground(false)
-    self:SetUpdateOnType(true)
+    -- self:SetUpdateOnType(true)
     self:SetTextColor(TLib2.Colors.Base4)
     self:SetCursorColor(TLib2.Colors.Base3)
     self:SetHighlightColor(TLib2.Colors.Base2)
 
     self.Up.Paint = function(dPanel, iW, iH)
-        -- draw.SimpleText("▲", "TLib2.7", (iW * 0.5), (iH * 0.5), TLib2.Colors.Base4, 1, 1)
         TLib2.DrawFAIcon("f0d8", "TLib2.FA.8", (iW * 0.5), (iH * 0.5), dPanel:IsHovered() and TLib2.Colors.Base4 or TLib2.Colors.Base3, 1, 1)
     end
 
     self.Down.Paint = function(dPanel, iW, iH)
-        -- draw.SimpleText("▼", "TLib2.7", (iW * 0.5), (iH * 0.5), TLib2.Colors.Base4, 1, 1)
         TLib2.DrawFAIcon("f0d7", "TLib2.FA.8", (iW * 0.5), (iH * 0.5), dPanel:IsHovered() and TLib2.Colors.Base4 or TLib2.Colors.Base3, 1, 1)
     end
 
@@ -26,11 +24,23 @@ function PANEL:Init()
 end
 
 function PANEL:PerformLayout(iW, iH)
-	self.Up:SetSize(iH, (iH * 0.5))
+    local iUpW, iUpH = self.Up:GetSize()
+    if (iUpW ~= iH) then
+        self.Up:SetWide(iH)
+    end
+    if (iUpH ~= (iH * 0.5)) then
+        self.Up:SetTall(iH * 0.5)
+    end
 	self.Up:AlignRight(1)
 	self.Up:AlignTop(1)
 
-	self.Down:SetSize(iH, (iH * 0.5))
+    local iDownW, iDownH = self.Down:GetSize()
+    if (iDownW ~= iH) then
+        self.Down:SetWide(iH)
+    end
+    if (iDownH ~= (iH * 0.5)) then
+        self.Down:SetTall(iH * 0.5)
+    end
 	self.Down:AlignRight(1)
 	self.Down:AlignBottom(1)
 end
