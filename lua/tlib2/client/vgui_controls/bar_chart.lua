0654b614a164282afc29c4c2d0ca2e502106bdba
Timmy <timmy.the.nobody@gmail.com>
2024-09-27 14:02:51 +0200

    chore (bar chart): UI tweaks

--- lua/tlib2/client/vgui_controls/bar_chart.lua
+++ lua/tlib2/client/vgui_controls/bar_chart.lua
@@ -1,8 +1,6 @@
 local PANEL = {}
 
 function PANEL:Init()
-    self:DockPadding(TLib2.Padding3, TLib2.Padding3, TLib2.Padding3, TLib2.Padding3)
-    self:DockMargin(0, 0, TLib2.Padding2, TLib2.Padding3)
     self.data = {}
 
     self:SetXAxis({
@@ -10,18 +8,51 @@ function PANEL:Init()
         max = 100,
         steps = 10,
         format_step = function(iPerc, iStep)
-            return math.Round(iPerc)
+            return math.Round(iPerc, 1)
         end
     })
 
     self:SetYAxis({
         visible = true,
         labels_enabled = true,
-        label_width = 0.25,
         format_value = function(xKey, xValue)
             return xValue
         end
     })
+
+    self.bottom = self:Add("DPanel")
+    self.bottom:Dock(BOTTOM)
+    self.bottom:SetTall(TLib2.VGUIControlH2)
+    self.bottom.Paint = function(_, iW, iH)
+        if not self.right or not self.right:IsValid() then return end
+
+        local iRightW = self.right:GetWide()
+        surface.SetDrawColor(TLib2.Colors.Base3)
+    
+        for i = 0, self.x_axis.steps do
+            local iX = self.left:GetWide() + (self.right:GetWide() * (i / (self.x_axis.steps))) - 1
+
+            local bOldClipping = DisableClipping(true)
+            draw.SimpleText(self.x_axis.format_step(((self.x_axis.max / self.x_axis.steps) * i), i), "TLib2.7", iX, iH, TLib2.Colors.Base3, 1, 4)
+            DisableClipping(bOldClipping)
+        end
+    end
+    self.left = self:Add("DPanel")
+    self.left:Dock(LEFT)
+    self.left:SetWide(ScrH() * 0.2)
+    self.left.Paint = nil
+
+    self.right = self:Add("DPanel")
+    self.right:Dock(FILL)
+    self.right:DockMargin(0, 0, TLib2.Padding2, 0)
+    self.right.Paint = function(_, iW, iH)    
+        surface.SetDrawColor(TLib2.Colors.Base3)
+
+        for i = 0, self.x_axis.steps do
+            local iX = math.floor(iW * (i / (self.x_axis.steps))) - ((i == 0) and 0 or 1)
+            surface.DrawLine(iX, 0, iX, iH)
+        end
+    end
 end
 
 function PANEL:SetXAxis(tAxis)
@@ -40,52 +71,52 @@ end
 
 function PANEL:AddData(sLabel, fValue)
     local iKey = (#self.data + 1)
-    self.data[iKey] = {label = sLabel, value = fValue}
+    self.data[iKey] = {
+        label = sLabel,
+        value = fValue,
+        line_panel = self.right:Add("DPanel")
+    }
 
-    local dLine = self:Add("DPanel")
-    self.data[iKey].line = dLine
+    local dDataLine = self.data[iKey].line_panel
+    dDataLine:Dock(TOP)
+    dDataLine:SetTall(TLib2.VGUIControlH2)
+    dDataLine.Paint = nil
 
-    dLine:Dock(TOP)
-    dLine:SetTall(TLib2.VGUIControlH2)
-    dLine:DockMargin(0, 0, 0, TLib2.Padding4)
-    dLine.Paint = nil
-    dLine.PerformLayout = function(_, iW, iH)
-        if not dLine.label or not dLine.label:IsValid() then return end
-        if not dLine.progress or not dLine.progress:IsValid() then return end
+    local dProgress = dDataLine:Add("DPanel")
+    dProgress:Dock(LEFT)
+    dProgress:DockPadding(0, TLib2.Padding4, 0, TLib2.Padding4)
+    dProgress.approach_w = 0
+    dProgress.bar_h = 0.5
 
-        local iCurXLabelW = dLine.label:GetWide()
-        local iXLabelW = (iW * self.y_axis.label_width)
-        if (iCurXLabelW ~= iXLabelW) then
-            dLine.label:SetSize(iXLabelW)
+    dProgress.PerformLayout = function(_, iW, iH)
+        local iProgressW = self.right:GetWide() * (fValue / self.x_axis.max)
+        if (iW ~= iProgressW) then
+            dProgress:SetWide(math.max(iProgressW, 3))
         end
-
-        local iProgressW = (iW - iCurXLabelW) * (fValue / self.x_axis.max)
-        if (dLine.progress:GetWide() ~= iProgressW) then
-            dLine.progress:SetWide(iProgressW)
-        end
-
-        print("rewrite")
     end
 
-    dLine.label = dLine:Add("DLabel")
-    dLine.label:Dock(LEFT)
-    dLine.label:SetFont("TLib2.7")
-    dLine.label:SetTextColor(TLib2.Colors.Base4)
-    dLine.label:SetContentAlignment(6)
-    dLine.label:SetTextInset(TLib2.Padding2, 0)
-    dLine.label:SetText(sLabel)
-
-    dLine.progress = dLine:Add("DPanel")
-    dLine.progress:Dock(FILL)
-    dLine.progress.lerp_w = 0
-    dLine.progress.Paint = function(_, iW, iH)
-        dLine.progress.lerp_w = math.Approach(dLine.progress.lerp_w, iW, FrameTime() * 256)
-
-        surface.SetDrawColor(dLine.progress:IsHovered() and TLib2.Colors.Action or TLib2.Colors.Accent)
-        surface.DrawRect(0, 0, dLine.progress.lerp_w, iH)
-
-        draw.SimpleText(self.y_axis.format_value(iKey, fValue), "TLib2.7", math.min(dLine.progress.lerp_w, iW) - TLib2.Padding4, iH * 0.5, TLib2.Colors.Base4, 2, 1)
+    dDataLine.PerformLayout = function(_, iW, iH)
+        dProgress:InvalidateLayout()
     end
+
+    dProgress.Paint = function(_, iW, iH)
+        local bHovered = dProgress:IsHovered()
+        local fBarH = (dProgress.bar_h * iH)
+
+        dProgress.approach_w = math.Approach(dProgress.approach_w, iW - 2, RealFrameTime() * 1280)
+        dProgress.approach_w = math.min(dProgress.approach_w, iW)
+
+        surface.SetDrawColor(bHovered and TLib2.Colors.Action or TLib2.Colors.Accent)
+        surface.DrawRect(1, (iH - fBarH) * 0.5, dProgress.approach_w, fBarH)
+
+        draw.SimpleText(self.y_axis.format_value(iKey, fValue), "TLib2.6", dProgress.approach_w - TLib2.Padding4, (iH * 0.5), TLib2.Colors.Base4, 2, 1)
+
+        local bOldClipping = DisableClipping(true)
+            draw.SimpleText(sLabel, "TLib2.7", -TLib2.Padding2, (iH * 0.5), bHovered and TLib2.Colors.Base4 or TLib2.Colors.Base3, 2, 1)
+        DisableClipping(bOldClipping)
+    end
+
+    self:InvalidateLayout()
 
     return iKey
 end
@@ -96,8 +127,9 @@ function PANEL:RemoveData(iKey)
         if (i ~= iKey) then
             tNewData[#tNewData + 1] = self.data[i]
         else
-            if self.data[i].line and self.data[i].line:IsValid() then
-                self.data[i].line:Remove()
+            if self.data[i].line_panel and self.data[i].line_panel:IsValid() then
+                self.data[i].line_panel:Remove()
+                self:InvalidateLayout()
             end
         end
     end
@@ -105,33 +137,24 @@ function PANEL:RemoveData(iKey)
 end
 
 function PANEL:Paint(iW, iH)
-    local iXLabelW = (iW * self.y_axis.label_width)
-
-    surface.SetDrawColor(TLib2.Colors.Base1)
-    surface.DrawRect(0, 0, iW, iH)
-
-    surface.SetDrawColor(TLib2.Colors.Base3)
-
-    for i = 0, (self.x_axis.steps) do
-        local iX = ((iW - iXLabelW - TLib2.Padding2) * ((i) / (self.x_axis.steps))) + iXLabelW - 1
-
-        surface.DrawLine(iX, 0, iX, iH - (TLib2.Padding4 * 5))
-        draw.SimpleText(self.x_axis.format_step(((self.x_axis.max / self.x_axis.steps) * i), i), "TLib2.7", iX, iH, TLib2.Colors.Base3, 1, 4)
-    end
 end
 
 function PANEL:PerformLayout(iW, iH)
     local iTall = 0
-
     for i = 1, #self.data do
-        local dLine = self.data[i].line
+        local dLine = self.data[i].line_panel
         if dLine and dLine:IsValid() then
             local _, iMT, _, iMB = dLine:GetDockMargin()
             iTall = iTall + dLine:GetTall() + iMT + iMB
         end
     end
-
-    iTall = iTall + (TLib2.Padding4 * 8)
+    
+    if self.y_axis.labels_enabled then
+        if self.bottom and self.bottom:IsValid() then
+            self.bottom:InvalidateLayout()
+            iTall = iTall + self.bottom:GetTall()
+        end
+    end
 
     if (iH ~= iTall) then
         self:SetTall(iTall)
