788d74d0393b77b9418bfe750fbac8d24465bc20
Timmy <timmy.the.nobody@gmail.com>
2024-10-23 08:52:47 +0200

    feat (tooltip): add anchor dire notion/methods

--- lua/tlib2/client/vgui_controls/tooltip.lua
+++ lua/tlib2/client/vgui_controls/tooltip.lua
@@ -38,6 +38,7 @@ hook.Add("Think", "TLib2:Tooltip:Think", function()
     dDrawnTT = vgui.Create("TLib2:Tooltip")
     dDrawnTT:SetText(TLib2.__tooltips[dHovered])
     dDrawnTT:SetAnchor(dHovered)
+    dDrawnTT:SetAnchorDir(BOTTOM)
 
     function dDrawnTT:Think()
         if not dHovered:IsValid() then
@@ -58,6 +59,21 @@ end)
 -- Control
 ----------------------------------------------------------------------
 
+local tAnchorDirs = {
+    [TOP] = function(iAX, iAY, iAW, iAH, iTTW, iTTH)
+        return iAX + ((iAW - iTTW) * 0.5), iAY - iTTH - (TLib2.Padding4 * 2)
+    end,
+    [BOTTOM] = function(iAX, iAY, iAW, iAH, iTTW, iTTH)
+        return iAX + ((iAW - iTTW) * 0.5), iAY + iAH + (TLib2.Padding4 * 2)
+    end,
+    [LEFT] = function(iAX, iAY, iAW, iAH, iTTW, iTTH)
+        return iAX - iTTW - (TLib2.Padding4 * 2), iAY + ((iAH - iTTH) * 0.5)
+    end,
+    [RIGHT] = function(iAX, iAY, iAW, iAH, iTTW, iTTH)
+        return iAX + iAW + (TLib2.Padding4 * 2), iAY + ((iAH - iTTH) * 0.5)
+    end
+}
+
 local PANEL = {}
 
 local draw = draw
@@ -66,36 +82,55 @@ local DisableClipping = DisableClipping
 
 function PANEL:Init()
     self:SetAlpha(0)
+    self:AlphaTo(255, 0.2, 0)
+
     self:SetDrawOnTop(true)
     self:SetFont("TLib2.7")
     self:SetContentAlignment(5)
     self:SetTextColor(TLib2.Colors.Base4)
     self:SetWrap(true)
     self:SetAutoStretchVertical(true)
-    self:AlphaTo(255, 0.2, 0)
 
     self.padding = TLib2.Padding3
+    self.anchor_dir = TOP
 end
 
+function PANEL:Think()
+    if self.anchor then
+        if not self.anchor:IsValid() then
+            self:Remove()
+        end
+    end
+end
+
+---`🔸 Client`<br>
+---Returns the anchor panel
+---@return Panel? @Anchor panel, or `nil` if none
 function PANEL:GetAnchor()
     return self.anchor
 end
 
-function PANEL:SetAnchor(dPanel)
-    self:Hide()
-    self.anchor = dPanel
-
-    local fnOldOnRemove = dPanel.OnRemove
-    dPanel.OnRemove = function(_)
-        self:Remove()
-
-        if (type(fnOldOnRemove) == "function") then
-            fnOldOnRemove(dPanel)
-        end
-    end
-
+---`🔸 Client`<br>
+---Sets the anchor panel
+---@param xPanel Panel|nil @Anchor panel, or `nil` to clear 
+function PANEL:SetAnchor(xPanel)
+    self.anchor = ispanel(xPanel) and xPanel or nil
     self:InvalidateLayout(true)
-    self:Show()
+end
+
+---`🔸 Client`<br>
+---Returns the anchor direction
+---@return number @Anchor direction (TOP, BOTTOM, LEFT, RIGHT)
+function PANEL:GetAnchorDir()
+    return self.anchor_dir
+end
+
+---`🔸 Client`<br>
+---Sets the anchor direction
+---@param iDir number @Anchor direction (TOP, BOTTOM, LEFT, RIGHT)
+function PANEL:SetAnchorDir(iDir)
+    self.anchor_dir = tAnchorDirs[iDir] and iDir or TOP
+    self:InvalidateLayout(true)
 end
 
 function PANEL:Paint(iW, iH)
@@ -121,10 +156,19 @@ function PANEL:PerformLayout(iW, iH)
         self:SetWide(iNewW)
     end
 
-    if self.anchor and self.anchor:IsValid() then
-        local iX, iY = self.anchor:LocalToScreen(0, 0)
-        iX = iX + (self.anchor:GetWide() * 0.5) - (iNewW * 0.5)
-        iY = iY - self:GetTall() - (TLib2.Padding4 * 2)
+    local dAnchor = self:GetAnchor()
+    if dAnchor and dAnchor:IsValid() then
+        local iAnchX, iAnchY = dAnchor:LocalToScreen(0, 0)
+        local iAnchW, iAnchH = dAnchor:GetSize()
+
+        local iX, iY = tAnchorDirs[self:GetAnchorDir()](
+            iAnchX,
+            iAnchY,
+            iAnchW,
+            iAnchH,
+            iNewW,
+            iH
+        )
 
         if (self:GetX() ~= iX) or (self:GetY() ~= iY) then
             self:SetPos(iX, iY)
